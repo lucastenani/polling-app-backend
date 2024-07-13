4dc2c1cae42b5218d6c83340ee9950f27eac9e02
lucastenani <lucas.tenani3@gmail.com>
2024-07-13 10:46:41 -0300

    Config PostgreSQL with Docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import express from 'express'
 
 export const app = express()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    email: 'jOhn doe',
+    name: 'John Doe',
+  },
+})
