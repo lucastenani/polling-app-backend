4cea8c226acc378920a4f429214f46e57f140fde
lucastenani <lucas.tenani3@gmail.com>
2024-07-12 23:59:03 -0300

    ESLint config

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
-import {z} from 'zod'
+
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
@@ -8,11 +9,10 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
+if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
 
   throw new Error('Invalid environment variables')
 }
-
 
 export const env = _env.data
