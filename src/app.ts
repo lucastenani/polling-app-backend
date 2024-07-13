1ef252ade54a9e0fe2f7622096f74a730cacddd2
lucastenani <lucas.tenani3@gmail.com>
2024-07-13 19:03:31 -0300

    Enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import cors from 'cors'
 import express from 'express'
 
 import { appRoutes } from './http/routes'
 
 export const app = express()
+app.use(cors())
 app.use(express.json())
 
 appRoutes(app)
